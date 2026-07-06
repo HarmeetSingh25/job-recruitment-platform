5cb5be43b6e4db472ee0f6031cc0e4e1696814d2
HarmeetSingh25 <harmeetsingh6p@gmail.com>
2026-07-07 03:16:31 +0530

    fix: Update CORS configuration and correct axios baseURL assignment

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,14 +9,13 @@ import duplicateRouter from "./routes/duplicateJob.routes.js";
 dotenv.config();
 
 const app = express();
-// app.use(
-//   cors({
-//     origin: ["http://localhost:5173","https://job-recruitment-platform-1auidkcwt-codes-h7s-projects.vercel.app/"],
-//     credentials: true,
-//   })
-// )
+app.use(
+  cors({
+    origin: "https://job-recruitment-platform-1auidkcwt-codes-h7s-projects.vercel.app/",
+    credentials: true,
+  })
+)
 
-app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 app.use("/api/jobs", jobRoutes);
--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: "http://localhost:3000/api"|| import.meta.env.VITE_API_URL,
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
