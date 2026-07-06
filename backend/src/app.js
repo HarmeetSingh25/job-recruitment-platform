13b586edad397bc15a33ce162a3b9bd0b4e22a7d
HarmeetSingh25 <harmeetsingh6p@gmail.com>
2026-07-07 02:41:12 +0530

    fix: Update CORS origin to use the correct deployment URL

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,7 +10,7 @@ import duplicateRouter from "./routes/duplicateJob.routes.js";
 const app = express();
 app.use(
   cors({
-    origin: ["http://localhost:5173","https://job-recruitment-platform-eta.vercel.app/"],
+    origin: ["http://localhost:5173","https://job-recruitment-platform-1auidkcwt-codes-h7s-projects.vercel.app/"],
     credentials: true,
   })
 )
